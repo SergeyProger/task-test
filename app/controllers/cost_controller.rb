508254d6877aac8816db6554c0fb1f2d28f39b9b
Sergey <sergeykogut1@gmail.com>
2018-11-19 14:12:40 +0200

    test conflict

--- app/controllers/cost_controller.rb
+++ app/controllers/cost_controller.rb
@@ -1,6 +1,6 @@
 class CostController < ApplicationController
   protect_from_forgery with: :null_session
-
+# comit conflict branch
   def index
     if params[:cost]
       convert = TextConvert.new
